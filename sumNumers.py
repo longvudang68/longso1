bf1d0694fada230fbcd69c27824b546ff7d9761c
longvudang68 <65530922+longvudang68@users.noreply.github.com>
2020-05-25 14:25:56 +0700

    add new project

--- sumNumers.py
+++ sumNumers.py
@@ -1,5 +1,19 @@
-def sumNumers(list):
-    sum=0
-    for number in list:
-        sum+=number
-    return sum
+import random
+import string
+def matKhau(a):
+    doDai = string.ascii_letters + string.punctuation  + string.digits
+
+
+
+    return ''.join(random.choice(doDai) for i in range(a))
+
+def checkMatKhau(b):
+    n =[]
+    n = slice.
+
+
+print("First Random String is ", matKhau(8))
+a = matKhau(8)
+print("second Random String is ", matKhau(8))
+
+
